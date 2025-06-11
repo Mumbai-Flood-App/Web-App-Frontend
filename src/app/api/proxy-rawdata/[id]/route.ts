a58b93bf904a52dea30b2773677f2eaecbaac2de
muhammed rashid <muhammedrashid.edu@gmail.com>
2025-06-12 04:48:47 +0530

    added rainfall plots and about page

--- src/app/api/proxy-rawdata/[id]/route.ts
+++ src/app/api/proxy-rawdata/[id]/route.ts
@@ -1,11 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
-  const { id } = context.params;
-  const response = await fetch(`https://api.mumbaiflood.in/aws/stations/${id}/rawdata/`);
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  const response = await fetch(`https://api.mumbaiflood.in/aws/stations/${params.id}/rawdata/`);
   const data = await response.json();
   return NextResponse.json(data);
 } 

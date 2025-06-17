9fb1fd4cd9259474698e7c4cd3dc50f70be4fd65
muhammed rashid <muhammedrashid.edu@gmail.com>
2025-06-17 16:37:59 +0530

    updated legend

--- src/components/FooterLogos.tsx
+++ src/components/FooterLogos.tsx
@@ -13,30 +13,6 @@ const logos = [
 export default function FooterLogos() {
   return (
     <>
-      {/* Mobile View - Bottom Horizontal */}
-      <div className="md:hidden fixed bottom-0 right-0 z-30 bg-white rounded-tl-lg px-8 py-1.5 items-center gap-8 shadow-lg flex"
-           style={{ minHeight: 50 }}>
-        {logos.map((logo, i) => (
-          <Image
-              key={i}
-              src={logo.src}
-              alt={logo.alt}
-              width={
-              logo.alt === "CCS" ? 60 :
-              logo.alt === "IMD" ? 25 :
-              35
-              }
-              height={
-              logo.alt === "CCS" ? 35 :
-              logo.alt === "IMD" ? 15 :
-              20
-              }
-              className="object-contain"
-              priority={i === 0}
-          />
-        ))}
-      </div>
-
       {/* Desktop View - Top Right Vertical */}
       <div className="hidden md:flex fixed top-0 right-0 z-30 bg-white rounded-bl-lg px-4 py-3 flex-col items-center gap-4 shadow-lg"
            style={{ minWidth: 80 }}>

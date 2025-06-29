07817ce787b85295f82468929c77260f035a0cf4
muhammed rashid <muhammedrashid.edu@gmail.com>
2025-06-29 12:52:22 +0530

    updated reported flood page

--- src/components/Sidebar/LanguageIcon.tsx
+++ src/components/Sidebar/LanguageIcon.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 
@@ -7,32 +6,28 @@ const LanguageIcon = ({ open }: { open?: boolean }) => {
   const isActive = pathname === '/';
 
   return open ? (
-    <Link href="/">
-      <div className="relative w-[200px] h-[36px] cursor-pointer">
-        <span className={`absolute top-[8px] left-[49px] font-medium ${isActive ? 'text-white' : 'text-[#8dadff]'}`}>Language</span>
-        <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[28.57%] h-[36px] overflow-hidden">
-          <Image
-            src={isActive ? '/icons/language-open-white.svg' : '/icons/language-open.svg'}
-            alt="Language"
-            width={36}
-            height={36}
-            className="object-contain"
-          />
-        </div>
-      </div>
-    </Link>
-  ) : (
-    <Link href="/language">
-      <div className="w-[36px] h-[36px] flex items-center justify-center relative cursor-pointer">
+    <div className="relative w-[200px] h-[36px]">
+      <span className="absolute top-[8px] left-[49px] font-medium text-[#8dadff]">Language</span>
+      <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[28.57%] h-[36px] overflow-hidden">
         <Image
+          src="/icons/language-open.svg"
+          alt="Language"
           width={36}
           height={36}
-          alt="Language"
-          src={isActive ? '/icons/language-closed-white.svg' : '/icons/language.svg'}
-          className="object-contain absolute top-0 left-0 w-[36px] h-[36px]"
+          className="object-contain"
         />
       </div>
-    </Link>
+    </div>
+  ) : (
+    <div className="w-[36px] h-[36px] flex items-center justify-center relative">
+      <Image
+        width={36}
+        height={36}
+        alt="Language"
+        src="/icons/language-open.svg"
+        className="object-contain absolute top-0 left-0 w-[36px] h-[36px]"
+      />
+    </div>
   );
 };
 
